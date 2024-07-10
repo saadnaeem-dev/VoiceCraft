b6508aa777764ae8bca935f267eac99068cb743b
saadnaeem-dev <saadnaeem.dev@gmail.com>
2024-07-10 21:19:09 +0500

    Refined DockerFile-4

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -16,7 +16,38 @@ docker run -it \
     -e GRANT_SUDO=yes \
     -w "/home/${NB_USER}" \
     -v "$PWD":"/home/$USER/work" \
-    voicecraft
+    voicecraft-3
 
 ## `docker logs jupyter` to get the URL link and token e.g.
 ## http://127.0.0.1:8888/lab?token=blahblahblahblabhlaabhalbhalbhal
+
+# Set the environment variables
+# Set the environment variables
+$env:NB_USER = $env:USERNAME
+$env:CHOWN_HOME = "yes"
+$env:GRANT_SUDO = "yes"
+
+# Check if the container is running
+$container = docker ps -q -f "name=jupyter"
+
+if (-not $container) {
+    # If the container is not running, run it
+    docker run -it `
+        -d `
+        --gpus all `
+        -p 8888:8888 `
+        -p 7860:7860 `
+        --name jupyter `
+        --user root `
+        -e NB_USER=$env:NB_USER `
+        -e CHOWN_HOME=$env:CHOWN_HOME `
+        -e GRANT_SUDO=$env:GRANT_SUDO `
+        -w "/home/$($env:NB_USER)" `
+        -v "$(Get-Location):/home/$($env:NB_USER)/work" `
+        voicecraft-3
+} else {
+    # If the container is running, start it
+    docker start jupyter
+}
+
+
